c19ad066f58004cb7f3549d3d6a3114eed8b8092
WD <52528405+witaliden@users.noreply.github.com>
2022-10-06 20:33:22 +0200

    Zadania 1 i 2

---
+++ src/lab1_szyfrowanieCezara.java
@@ -0,0 +1,51 @@
+
+import java.util.Scanner;
+
+public class lab1_szyfrowanieCezara {
+    private static final char LETTER_A = 'a';
+    private static final char LETTER_Z = 'z';
+    private static final int ALPHABET_SIZE = LETTER_Z - LETTER_A + 1;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nPodaj wiadomość do zaszyfrowania: ");
+        String message = sc.nextLine();
+
+        System.out.println("Podaj podstawę do szyfrowania: ");
+        int encryptKey = sc.nextInt();
+        System.out.println("Podaj drugą podstawę do szyfrowania: ");
+        int encryptKey2 = sc.nextInt();
+
+        String encodedMessage = encodeMessage(message, encryptKey);
+        System.out.println("\nZaszyfrowana wiadomość:");
+        System.out.println(encodedMessage);
+
+        String encodedMessage2 = encodeMessage(encodedMessage, encryptKey2);
+        System.out.println("\nZaszyfrowana wiadomość 2:");
+        System.out.println(encodedMessage2);
+
+        System.out.println("\nDeszyfrowana wiadomość:");
+        String decodedMessage = decodeMessage(encodedMessage2, encryptKey2);
+        System.out.println(decodedMessage);
+        System.out.println("\nDeszyfrowana wiadomość:");
+        System.out.println(decodeMessage(decodedMessage, encryptKey));
+    }
+
+    public static String encodeMessage(String message, int key) {
+        StringBuilder result = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            if (!Character.isAlphabetic(c)) result.append(c);
+            else {
+                int originalAlphabetPosition = c - LETTER_A;
+                int newAlphabetPosition = (originalAlphabetPosition + key) % ALPHABET_SIZE;
+                result.append((char) (LETTER_A + newAlphabetPosition));
+            }
+        }
+        return result.toString();
+    }
+
+    public static String decodeMessage(String message, int key) {
+        return encodeMessage(message, ALPHABET_SIZE - (key % ALPHABET_SIZE));
+    }
+}
---
+++ src/lab2_vigenere.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class lab2_vigenere {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("\nPodaj wiadomość do zaszyfrowania: ");
+        String message = sc.nextLine().toUpperCase();
+
+        System.out.println("Podaj podstawę do szyfrowania: ");
+        String encryptKey = sc.nextLine().toUpperCase();
+
+        String encodedMessage = encodeMessage(message, encryptKey);
+        System.out.println("\nZaszyfrowana wiadomość:");
+        System.out.println(encodedMessage);
+
+        System.out.println("\nDeszyfrowana wiadomość:");
+        System.out.println(decodeMessage(encodedMessage, encryptKey));
+    }
+
+
+    private static String decodeMessage(String encodedMessage, String encryptKey) {
+        StringBuilder result = new StringBuilder();
+        encodedMessage = encodedMessage.toUpperCase();
+
+        for (int i = 0, j = 0; i < encodedMessage.length(); i++) {
+            char letter = encodedMessage.charAt(i);
+            if (!Character.isAlphabetic(letter)) result.append(letter);
+            else {
+                result.append((char) ((letter - encryptKey.charAt(j) + 26) % 26 + 65));
+                j = ++j % encryptKey.length();
+            }
+        }
+        return result.toString().toLowerCase();
+    }
+
+    private static String encodeMessage(String message, String encryptKey) {
+        StringBuilder result = new StringBuilder();
+        message = message.toUpperCase();
+        for (int i = 0, j = 0; i < message.length(); i++) {
+            char letter = message.charAt(i);
+            if (!Character.isAlphabetic(letter)) result.append(letter);
+            else {
+                result.append((char) (((letter - 65) + (encryptKey.charAt(j) - 65)) % 26 + 65));
+                j = ++j % encryptKey.length();
+            }
+        }
+        return result.toString().toLowerCase();
+    }
+}
